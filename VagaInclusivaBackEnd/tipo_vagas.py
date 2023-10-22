c1b5391c2ee66dbe81dbd1f23e7713b1cb67bb53
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2023-10-22 14:38:36 -0300

    Add files via upload

---
+++ VagaInclusivaBackEnd/tipo_vagas.py
@@ -0,0 +1,9 @@
+from avaliacao.models import Acessibilidade
+
+
+Acessibilidade.create("Surdo")
+Acessibilidade.create("Cego")
+Acessibilidade.create("Mudo")
+
+
+#python manage.py shell < tipos_vagas.py
